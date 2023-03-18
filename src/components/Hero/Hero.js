8b1a9659445276cb7be3d8d4b3d95c099d336842
Marija Rubil <rubilmarija@gmail.com>
2023-03-18 10:42:25 +0100

    Added Hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import './Hero.css';
+import blob from '../../assets/marija.png'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className='header section__padding' id='home'>
+            <div className='header-content'>
+                <h1 className='gradient__text'>Hi, I'm Marija</h1>
+                <p>Front End Developer</p>
+                <p>Welcome to my website!</p>
+            </div>
+            <div className='header-image'>
+                <img src={blob} alt="blob" />
+            </div>
+        </div>
     )
 }
 
