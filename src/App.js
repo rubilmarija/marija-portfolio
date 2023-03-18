efc9d605c09001503b019bdd75f3cfcd734eabdf
Marija Rubil <rubilmarija@gmail.com>
2023-03-18 15:29:43 +0100

    Added About component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Hero />
       </div>
+      <About />
       <Footer />
     </div>
   );
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './About.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import useCollapse from 'react-collapsed';
 
 const About = () => {
+    const [isExpanded, setExpanded] = useState(false);
+    const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
+
     return (
-        <div>About</div>
+        <div className='aboutMe section__margin' id='aboutMe'>
+
+            <div className='aboutMe-heading'>
+                <h1 className='gradient__text'>About me</h1>
+            </div>
+
+            <div className='aboutMe-feature'>
+                <p>I am Stockholm based Frontend Developer, passionate about bringing creativity to web.</p>
+            </div>
+
+            <div className='expand-section__about-me'>
+                <button
+                    {...getToggleProps({
+                        onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+                    })}
+                >
+                    {isExpanded ? <FontAwesomeIcon icon={faChevronUp} /> : <FontAwesomeIcon icon={faChevronDown} />}
+                </button>
+                <section {...getCollapseProps()}>
+                    <div>CV</div>
+                    <div><p>bal bla bla bla</p></div>
+                </section>
+            </div>
+
+        </div>
     )
 }
 
-export default About
+export default About;
