6c0849647c00d582e42ef68c645b7f1b259389b8
Marija Rubil <rubilmarija@gmail.com>
2023-04-02 18:05:13 +0200

    Update in css

---
+++ src/components/Tools/Tools.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Tools.css';
+import { FaFigma, FaNodeJs } from 'react-icons/fa';
+import { DiJavascript1 } from "react-icons/di";
+import { AiOutlineHtml5 } from "react-icons/ai";
+import { TbBrandCss3, TbBrandVscode, TbBrandBootstrap, TbBrandTailwind } from "react-icons/tb";
+import { RiReactjsFill } from "react-icons/ri";
+import { BsGit } from "react-icons/bs";
+
+const Tools = () => {
+    return (
+        <div className='tools-container section__margin'>
+            <div className='tools-heading'>
+                <h1 className='gradient__text'>Skills</h1>
+            </div>
+            <div className='aboutMe-feature'>
+                <p>Programming languages, libraries and tools that I am using in my projects, as well as collaboration projects</p>
+            </div>
+
+            <div className='icons'>
+                <div className='icon'> <AiOutlineHtml5 /></div>
+                <div className='icon'> <TbBrandCss3 /></div>
+                <div className='icon'> <DiJavascript1 /></div>
+                <div className='icon'> <RiReactjsFill /></div>
+                <div className='icon'> <FaNodeJs /></div>
+                <div className='icon'> <TbBrandVscode /></div>
+                <div className='icon'> <BsGit /></div>
+                <div className='icon'> <TbBrandBootstrap /></div>
+                <div className='icon'> <TbBrandTailwind /></div>
+                <div className='icon'> <FaFigma /></div>
+            </div>
+        </div>
+    )
+}
+
+export default Tools;
